9eaaa28a39bbdab0688f400db306a27ebf11a1d6
Daniel Monteiro <DanielMonteiro@criticalmanufacturing.com>
2025-07-22 15:35:01 +0000

    Initial mes bump mes scaffolding

--- cmf-cli/Commands/bump/BumpMESCommand.cs
+++ cmf-cli/Commands/bump/BumpMESCommand.cs
@@ -5,6 +5,7 @@ using Cmf.CLI.Core.Objects;
 using Cmf.CLI.Factories;
 using Cmf.CLI.Utilities;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
@@ -17,8 +18,8 @@ namespace Cmf.CLI.Commands
     ///
     /// </summary>
     /// <seealso cref="BaseCommand" />
-    [CmfCommand("bump", Id = "bump")]
-    public class BumpCommand : BaseCommand
+    [CmfCommand(name: "mes", Id = "bump_mes", ParentId = "bump")]
+    public class BumpMESCommand : BaseCommand
     {
         /// <summary>
         /// Configure command
@@ -31,20 +32,13 @@ namespace Cmf.CLI.Commands
                 getDefaultValue: () => { return new("."); },
                 description: "Package path"));
 
-            cmd.AddOption(new Option<string>(
-                aliases: new string[] { "-v", "--version" },
-                description: "Will bump all versions to the version specified"));
-
-            cmd.AddOption(new Option<string>(
-                aliases: new string[] { "-b", "--buildNr" },
-                description: "Will add this version next to the version (v-b)"));
-
-            cmd.AddOption(new Option<string>(
-                aliases: new string[] { "-r", "--root" },
-                description: "Will bump only versions under a specific root folder (i.e. 1.0.0)"));
+            cmd.AddArgument(new Argument<string>(
+                name: "MESVersion",
+                description: "New MES Version"
+            ));
 
             // Add the handler
-            cmd.Handler = CommandHandler.Create<DirectoryInfo, string, string, string>(Execute);
+            cmd.Handler = CommandHandler.Create<DirectoryInfo, string>(Execute);
         }
 
         /// <summary>
@@ -52,23 +46,16 @@ namespace Cmf.CLI.Commands
         /// </summary>
         /// <param name="packagePath">The package path.</param>
         /// <param name="version">The version.</param>
-        /// <param name="buildNr">The version for build Nr.</param>
-        /// <param name="root">The root.</param>
         /// <exception cref="CliException"></exception>
-        public void Execute(DirectoryInfo packagePath, string version, string buildNr, string root)
+        public void Execute(DirectoryInfo packagePath, string version)
         {
             using var activity = ExecutionContext.ServiceProvider?.GetService<ITelemetryService>()?.StartExtendedActivity(this.GetType().Name);
             IFileInfo cmfpackageFile = this.fileSystem.FileInfo.New($"{packagePath}/{CliConstants.CmfPackageFileName}");
 
-            if (string.IsNullOrEmpty(version) && string.IsNullOrEmpty(buildNr))
-            {
-                throw new CliException(string.Format(CliMessages.MissingMandatoryProperties, "version, buildNr"));
-            }
-
             // Reading cmfPackage
             CmfPackage cmfPackage = CmfPackage.Load(cmfpackageFile);
 
-            Execute(cmfPackage, version, buildNr, root);
+            Execute(cmfPackage, version);
         }
 
         /// <summary>
@@ -76,21 +63,13 @@ namespace Cmf.CLI.Commands
         /// </summary>
         /// <param name="cmfPackage">The CMF package.</param>
         /// <param name="version">The version.</param>
-        /// <param name="buildNr">The version for build Nr.</param>
-        /// <param name="root">The root.</param>
         /// <exception cref="CliException"></exception>
-        public void Execute(CmfPackage cmfPackage, string version, string buildNr, string root)
+        public void Execute(CmfPackage cmfPackage, string version)
         {
             IDirectoryInfo packageDirectory = cmfPackage.GetFileInfo().Directory;
             IPackageTypeHandler packageTypeHandler = PackageTypeFactory.GetPackageTypeHandler(cmfPackage);
 
-            // Will execute specific bump code per Package Type
-            Dictionary<string, object> bumpInformation = new()
-            {
-                { "root", root }
-            };
-
-            packageTypeHandler.Bump(version, buildNr, bumpInformation);
+            //packageTypeHandler.Bump(version, buildNr, bumpInformation);
 
             // will save with new version
             cmfPackage.SaveCmfPackage();
